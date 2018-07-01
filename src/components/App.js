a34f112bccfd12234bf333d94bffbc3cd49354bb
Domas <domas.bitvinskas@me.com>
2018-07-01 22:08:59 +0300

    Meta tags

--- src/components/App.js
+++ src/components/App.js
@@ -8,8 +8,13 @@ import {
 } from 'candour'
 import _ from 'lodash'
 import windowSize from 'react-window-size'
+import { Helmet } from 'react-helmet'
 
+import ogUrl from '../helpers/ogUrl'
 import Column from './Column'
+
+const title = 'Domas Bitvinskas - Maker & Startup Fanatic'
+const description = 'Just keep running better & better startups while others scream about how they are the best.'
 
 const startups = [
   {
@@ -163,6 +168,23 @@ class App extends Component {
   render() {
     return (
       <Container style={styles.containter}>
+        <Helmet>
+          <title>{title}</title>
+          <meta name='description' content={description} />
+          <meta property='og:title' content={title} />
+          <meta property='og:description' content={description} />
+
+          <meta property='og:image' content={`${ogUrl()}/ogimage.png`} />
+          <meta property='og:image:width' content='1200' />
+          <meta property='og:image:height' content='630' />
+          <meta property='og:image:type' content='image/jpg' />
+          <meta property='og:url' content={ogUrl()} />
+          <link rel='canonical' href={ogUrl()} />
+          <meta property='og:type' content='website' />
+          <meta property='og:locale' content='en_US' />
+          <meta property='og:site_name' content={title} />
+        </Helmet>
+
         <video
           style={styles.video}
           src='/hn.mp4'
